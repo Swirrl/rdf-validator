8f330925df9a2a346933b80a3a15d07eee691034
lkitching <lee@swirrl.com>
2018-09-04 15:20:03 +0100

    Define command line options.
    Define options for specifying test case files and SPARQL endpoint.
    Parse command line and display any errors.

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.7"]
+                 [grafter "0.11.4"]]
   :main ^:skip-aot rdf-validator.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/rdf_validator/core.clj
+++ src/rdf_validator/core.clj
@@ -1,7 +1,37 @@
 (ns rdf-validator.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [clojure.tools.cli :as cli]))
+
+(defn parse-endpoint [endpoint-str]
+  endpoint-str)
+
+(def cli-options
+  [["-s" "--suite SUITE" "Test suite file or directory"
+    :default []
+    :parse-fn io/file
+    :validate [(fn [f] true) "File does not exist"]
+    :assoc-fn (fn [m k v] (update-in m [k] conj v))]
+   ["-e" "--endpoint ENDPOINT" "SPARQL data endpoint to validate"
+    :parse-fn parse-endpoint]])
+
+(defn- usage [summary]
+  (println "Usage:")
+  (println summary))
+
+(defn- invalid-args [{:keys [errors summary]}]
+  (binding [*out* *err*]
+    (doseq [error errors]
+      (println error))
+    (println)
+    (usage summary)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [{:keys [errors] :as result} (cli/parse-opts args cli-options)]
+    (if (nil? errors)
+      (do
+        (println "RUNNING..."))
+      (do (invalid-args result)
+          (System/exit 1)))))
