07666c1ff3083c53cc09cb179afe837a3ad8d456
lkitching <lee@swirrl.com>
2018-09-04 16:12:21 +0100

    Parse endpoint string into repository.
    Parse the endpoint provided on the command line into a Repository
    instance. URIs with a http or https scheme are parsed as SPARQL
    repositories. All other values are considered to reference files or
    directories on disk which are loaded into an in-memory store.

--- src/rdf_validator/core.clj
+++ src/rdf_validator/core.clj
@@ -1,10 +1,46 @@
 (ns rdf-validator.core
   (:gen-class)
   (:require [clojure.java.io :as io]
-            [clojure.tools.cli :as cli]))
+            [clojure.tools.cli :as cli]
+            [grafter.rdf :as rdf]
+            [grafter.rdf.repository :as repo])
+  (:import [java.net URI URISyntaxException]
+           [java.io File]))
 
-(defn parse-endpoint [endpoint-str]
-  endpoint-str)
+(defn file->repository [^File f]
+  (if (.isDirectory f)
+    (let [r (repo/sail-repo)]
+      (println "Creating repository from directory: " (.getAbsolutePath f))
+      (doseq [df (.listFiles f)]
+        (rdf/add r (rdf/statements df)))
+      r)
+    (do
+      (println "Creating repository from file: " (.getAbsolutePath f))
+      (repo/fixture-repo f))))
+
+(defmulti uri->repository (fn [^URI uri] (some-> (.getScheme uri) keyword)))
+
+(defn- create-sparql-repo [uri]
+  (println "Creating SPARQL repository: " (str uri))
+  (repo/sparql-repo (str uri)))
+
+(defmethod uri->repository :http [uri]
+  (create-sparql-repo uri))
+
+(defmethod uri->repository :https [uri]
+  (create-sparql-repo uri))
+
+(defmethod uri->repository :file [uri]
+  (file->repository (io/file uri)))
+
+(defmethod uri->repository :default [uri]
+  (file->repository (io/file (str uri))))
+
+(defn- parse-endpoint [endpoint-str]
+  (try
+    (uri->repository (URI. endpoint-str))
+    (catch URISyntaxException ex
+      (file->repository (io/file endpoint-str)))))
 
 (def cli-options
   [["-s" "--suite SUITE" "Test suite file or directory"
@@ -32,6 +68,7 @@
   (let [{:keys [errors] :as result} (cli/parse-opts args cli-options)]
     (if (nil? errors)
       (do
-        (println "RUNNING..."))
+        (println "RUNNING...")
+        (System/exit 0))
       (do (invalid-args result)
           (System/exit 1)))))
