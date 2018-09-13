667ce973a59d6f14de1cda1bb9da3114bd0a551f
lkitching <lee@swirrl.com>
2018-09-13 12:32:14 +0100

    Configure logging.
    Issue #4 - Add SLF4j dependency and configure log4j2 as the backend.

--- project.clj
+++ project.clj
@@ -8,7 +8,13 @@
                  [org.clojure/tools.cli "0.3.7"]
                  [grafter "0.11.4"]
                  [org.apache.jena/apache-jena-libs "3.8.0" :extension "pom"]
-                 [selmer "1.12.0"]]
+                 [selmer "1.12.0"]
+                 [org.slf4j/slf4j-api "1.7.25"]
+
+                 ;;TODO: move logging dependencies when exposing as a library!
+                 [org.apache.logging.log4j/log4j-api "2.11.0"]
+                 [org.apache.logging.log4j/log4j-core "2.11.0"]
+                 [org.apache.logging.log4j/log4j-slf4j-impl "2.11.0"]]
   :main ^:skip-aot rdf-validator.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
