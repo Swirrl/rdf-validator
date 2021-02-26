c85338c44be9f7f9726c30dca4aa47ef8bd9cfe6
Rick Moynihan <rick.m@swirrl.com>
2021-02-26 14:44:51 +0000

    Fix #43 pretty print datatype values in table as RDF

--- src/rdf_validator/reporting.clj
+++ src/rdf_validator/reporting.clj
@@ -2,7 +2,8 @@
   "Used for creating reports of test executions."
   (:require [clojure.string :as string]
             [rdf-validator.util :as util]
-            [clojure.pprint :as pp]))
+            [clojure.pprint :as pp]
+            [grafter-2.rdf4j.io :as gio]))
 
 (defprotocol TestReporter
   (report-test-result! [this test-result]
@@ -10,13 +11,21 @@
   (report-test-summary! [this summary]
     "Reports a summary of all test executions"))
 
+(defn pp-rdf-val [v]
+  (if (uri? v)
+    (str "<" v ">")
+    (str (gio/->backend-type v))))
+
+(defn pprint-rdf-row [row]
+  (into {} (map (juxt key (comp pp-rdf-val val))) row))
+
 (defrecord ConsoleTestReporter []
   TestReporter
   (report-test-result! [_this {:keys [number test-source result errors] :as test-result}]
     (println (format "%d %s: %s" number (util/get-path test-source) (string/upper-case (name result))))
     (when (seq errors)
       (if (map? (first errors))
-        (pp/print-table errors)
+        (pp/print-table (map pprint-rdf-row errors))
         (doseq [error errors]
           (println (format "\t%s" error)))))
     (when (pos? (count errors))
